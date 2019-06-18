1455e273957170f6b0d68bddf67fe8becb206e57
miloradbecin2012 <milorad.becin2017@outlook.com>
2019-06-18 10:55:35 +0200

    add production configure

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,13 @@
+// export const AUTH_CONFIG = {
+//   domain: 'dream1025.auth0.com',
+//   clientID: '1XvjStlMHnJOieRLRkenbCz2Oa1rza01',
+//   callbackUrl: 'http://localhost:3000/callback'
+// }
+
+// production configure
+
 export const AUTH_CONFIG = {
   domain: 'dream1025.auth0.com',
   clientID: '1XvjStlMHnJOieRLRkenbCz2Oa1rza01',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://sociallogin-providers.herokuapp.com/callback'
 }
